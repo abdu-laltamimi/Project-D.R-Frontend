8c5809348a577e0aca260fad4fe86d1c150f5c34
Mohamed Fakroun <mohfakroun@gmail.com>
2025-02-16 17:06:05 +0000

    ++

--- src/components/OurPlans.jsx
+++ src/components/OurPlans.jsx
@@ -20,7 +20,7 @@ const OurPlans = () => {
           className="mb-24 text-center"
         >
           <motion.h2 
-            className="text-xl text-left md:text-center md:text-7xl font-normal text-[#6CA3A5]"
+            className="text-6xl text-left md:text-center md:text-7xl font-normal text-[#6CA3A5]"
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.3 }}
