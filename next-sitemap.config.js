2281ae67487acb535c1bec2704fd1b1c0b89f0ce
Mohamed Fakroun <mohfakroun@gmail.com>
2025-02-23 20:57:32 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://mymaternalhub.co.uk', // Change to your domain
+    generateRobotsTxt: true, // Creates a robots.txt file
+    exclude: ['/api/*'], // Pages to exclude (if any)
+  };
