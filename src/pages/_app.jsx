31fc3602a99eb977b81493c5b6e7d96803a7ba81
Mohamed Fakroun <mohfakroun@gmail.com>
2025-02-23 21:21:21 +0000

    app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,40 +8,14 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        {/* SEO Meta Tags */}
-        <title>My Maternal Hub | Midwife & Postnatal Services in Greater Manchester</title>
-        <meta 
-          name="description" 
-          content="Expert midwife and postnatal care services in Greater Manchester. Compassionate support for new mothers, home visits, and lactation consulting. Book now!" 
-        />
-        <meta 
-          name="keywords" 
-          content="midwife services Greater Manchester, postnatal care Greater Manchester, maternity support, home birth midwife, lactation consultant, newborn care, postnatal care, postnatal support, postnatal services, postnatal care services, postnatal care in Greater Manchester" 
-        />
-        <meta name="author" content="My Maternal Hub" />
         <meta name="theme-color" content="#27272a" />
-        
-        {/* Open Graph (Facebook) Meta Tags */}
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="My Maternal Hub | Midwife & Postnatal Care in Manchester" />
-        <meta property="og:description" content="Providing trusted midwife and postnatal care services in Greater Manchester. Compassionate and professional support for new mothers." />
-        <meta property="og:url" content="https://mymaternalhub.co.uk" />
-        <meta property="og:image" content="https://mymaternalhub.co.uk/images/babyCta.jpeg" />
-
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="My Maternal Hub | Midwife & Postnatal Services" />
-        <meta name="twitter:description" content="Get expert midwifery and postnatal care in Greater Manchester. Book a consultation today!" />
-        <meta name="twitter:image" content="https://mymaternalhub.co.uk/images/babyCta.jpeg" />
 
         {/* Favicon & App Icons */}
         <link rel="apple-touch-icon" sizes="180x180" href="/maternal.ico" />
-        <link rel="icon" type="image/x-icon" href="/maternal.ico" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/maternal.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/maternal.ico" />
+        <link rel="icon" href="/maternal.ico" />
 
-        {/* Canonical URL */}
-        <link rel="canonical" href="https://mymaternalhub.co.uk" />
       </Head>
       <NavBar />
       <Component {...pageProps} />
